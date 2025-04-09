22cab31acb15e6b972cc50df9f433e325a6f3cb3
himanshu singh <himanshu.singh@bcstechnology.com.au>
2025-04-09 12:07:45 +0530

    change in unit type to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,7 @@ const request = require('request')
 // })
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=083a12be18f8d4d8a114836dffb78762&query='+latitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=083a12be18f8d4d8a114836dffb78762&query='+latitude+','+longitude+'&units=m'
     request({ url, json: true }, (error, {body}) => {
     if(error){
         callback('Unable to connect to weather service', undefined);
